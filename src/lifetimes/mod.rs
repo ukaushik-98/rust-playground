b0109501665dae1e669dc20452821a4b66567f64
ukaushik-98 <kaushikuk101@gmail.com>
2025-01-19 08:19:51 -0800

    update

--- src/lifetimes/mod.rs
+++ src/lifetimes/mod.rs
@@ -1,30 +1,36 @@
-struct ByteIter<'a> {
-    remainder: &'a [u8],
+struct ByteIter<'remainder> {
+    remainder: &'remainder [u8],
 }
 
 impl<'a> ByteIter<'a> {
+    fn new(remainder: &'a [u8]) -> Self {
+        ByteIter { remainder }
+    }
+
     fn next<'mut_self>(&'mut_self mut self) -> Option<&'a u8> {
         if self.remainder.is_empty() {
             None
         } else {
-            let byte: &'a u8 = &self.remainder[0];
+            let result = &self.remainder[0];
             self.remainder = &self.remainder[1..];
-            Some(byte)
+            Some(result)
         }
     }
 }
 
 #[test]
-fn test_next() {
-    let mut bytes = ByteIter { remainder: b"1" };
-    assert_eq!(Some(&b'1'), bytes.next());
-    assert_eq!(None, bytes.next());
+fn test_empty() {
+    let remainder = b"";
+    let mut bytes = ByteIter::new(remainder);
+    assert_eq!(None, bytes.next().take());
 }
 
 #[test]
-fn test_next_iter() {
-    let mut bytes = ByteIter { remainder: b"1123" };
+fn test_iter() {
+    let remainder = b"1234";
+    let mut bytes = ByteIter::new(remainder);
     let byte_1 = bytes.next();
     let byte_2 = bytes.next();
-    assert_eq!(byte_1, byte_2);
+    assert_eq!(b'1', *byte_1.unwrap());
+    assert_eq!(b'2', *byte_2.unwrap());
 }
