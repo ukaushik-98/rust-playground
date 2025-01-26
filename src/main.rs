00b8552e1abe9ea9fbb03a1602cd14c65399dbd6
ukaushik-98 <kaushikuk101@gmail.com>
2025-01-26 12:46:24 -0800

    testing ref of mutex gaurd

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, MutexGuard};
 use std::thread;
 
 use rust_playground::variance;
 
+fn arc_fn<'a, T>(test: Arc<&'a MutexGuard<T>>) {
+    drop(*test);
+}
+
 fn main() {
     variance::tester();
-    let arc_dumb_gaurd = Arc::new(Mutex::new(10));
+    let x = Mutex::new(10);
 
     let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
     for _ in 0..10 {
         let counter = Arc::clone(&counter);
-        let arc_dumb_gaurd = Arc::clone(&arc_dumb_gaurd);
+        // let arc_dumb_gaurd = Arc::clone(&arc_dumb_gaurd);
         let handle = thread::spawn(move || {
-            println!("{:?}", arc_dumb_gaurd);
+            // println!("{:?}", arc_dumb_gaurd);
             let mut num = counter.lock().unwrap();
 
             *num += 1;
