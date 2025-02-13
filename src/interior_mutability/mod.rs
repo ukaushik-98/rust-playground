35699a088ef1341044b689f98814a0566ea7f1e0
ukaushik-98 <kaushikuk101@gmail.com>
2025-02-12 17:50:59 -0800

    update

--- src/interior_mutability/mod.rs
+++ src/interior_mutability/mod.rs
@@ -3,7 +3,7 @@ use std::cell::Cell;
 fn foo() {
     let mut x = vec![1, 2, 3];
     let y = &mut x;
-    // ✅ can't borrow x again as mut after mut borrow
+    // ❌ can't borrow x again as mut after mut borrow
     // let z = &mut x;
     y.push(4);
     x.push(5);
@@ -11,20 +11,57 @@ fn foo() {
 
 fn foo2() {
     let mut x = vec![1, 2, 3];
+    let y = &x;
+    // ❌ can't borrow x again as mut after mut borrow
+    // let z = &mut x;
+    //  can't borrow x as mut since immutable pointer exists
+    // x.push(5);
+    println!("{:?}", y)
+}
+
+fn foo3() {
+    let mut x = vec![1, 2, 3];
     let mut y = Cell::new(&mut x);
-    // ✅ can't borrow x again after placing mut ref into cell
+    // ❌ can't borrow x again after placing mut ref into cell
     // let a = &mut x;
     let z = y.get_mut();
     z.push(4);
     x.push(5);
 }
 
-fn foo3() {
+fn foo4() {
     let mut x = vec![1, 2, 3];
-    let y = &x;
-    // ✅ can't borrow x again as mut after mut borrow
-    // let z = &mut x;
-    //  can't borrow x as mut since immutable pointer exists
+    let mut y = Cell::new(&mut x);
+
+    // ❌ can't borrow x again after placing mut ref into cell
+    // let a = &mut x;
+    let z = y.get_mut();
+
+    // ❌ can't borrow as mut again
     // x.push(5);
-    println!("{:?}", y)
+    z.push(4);
+}
+
+fn foo5(_x: &mut Vec<i32>) {
+    let mut x = 5;
+    let mut y = Cell::new(x);
+    // get only available when T in Cell is copy
+    let z = y.get();
+}
+
+fn foo6() -> Vec<i32> {
+    let mut x = vec![1, 2, 3];
+    let y = Cell::new(&mut x);
+    bar(y);
+    x.push(5);
+    x
+}
+
+fn bar(mut a: Cell<&mut Vec<i32>>) {
+    a.get_mut().push(4);
+}
+
+#[test]
+fn foo6_test() {
+    assert_eq!(foo6(), vec![1, 2, 3, 4, 5]);
 }
